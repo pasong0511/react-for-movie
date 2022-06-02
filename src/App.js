5a4bdced8f1f0dc49e3df7a5fdb3ef3afb2ac712
pasong0511 <pasong0511@gmail.com>
2022-06-02 12:46:49 +0900

    디테일 페이지 데이터 요청
    영화 제목 클릭 시 id이용해서 단독 API 데이터 패칭

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 /*Movie.js*/
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Detail from "./routes/Detail";
 import Home from "./routes/Home";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/movie/:id" element={<Detail />} />
         <Route path="/" element={<Home />} />
       </Routes>
     </BrowserRouter>
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,11 +1,14 @@
 /*Movie.js*/
 import PropTypes from "prop-types"
+import { Link } from "react-router-dom";
 
 function Movie({ id, coverImg, title, summary, genres }) {
     return (
         <div>
             <img src={coverImg} alt={title} />
-            <h2>{title}</h2>
+            <h2>
+                <Link to={`/movie/${id}`}>{title}</Link>
+            </h2>
             <p>{summary}</p>
             <ul>
                 {genres.map((g) => (
@@ -18,6 +21,7 @@ function Movie({ id, coverImg, title, summary, genres }) {
 
 //propTypes 정의
 Movie.propTypes = {
+    id: PropTypes.number.isRequired,
     coverImg: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
     summary: PropTypes.string.isRequired,
